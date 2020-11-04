06af1fd939a0492782e9fc73e4877341b26ec670
Taylor Niles <tniles320@gmail.com>
2020-11-03 18:43:43 -0800

    functionality almost complete, styling still needed

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,19 @@
-//create connection to mysql server
+const mysql = require("mysql");
 
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Tt032091!",
+  database: "burgers_db"
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 module.exports = connection;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,56 @@
 const express = require("express");
 const burger = require("../models/burger");
 
-//create router
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    burger.all((data) => {
+      const burgerObject = {
+        burgers: data
+      };
+      console.log(burgerObject);
+      res.render("index", burgerObject);
+    });
+  });
+  
+  router.post("/api/burgers", (req, res) => {
+    burger.create([
+      "burger_name"
+    ], [
+      req.body.burger_name,
+    ], function(result) {
+      // Send back the ID of the new burger
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", (req, res) => {
+    const condition = `id = ${req.params.id}`;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      devoured: true
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  router.delete("/api/burgers", (req, res) => {
+    const condition = `id = ${req.params.id}`
+    
+    burger.delete(condition, (result) => {
+        if(result.affectedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+  });
 
 module.exports = router;
