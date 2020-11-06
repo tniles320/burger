a54e39d90093e1926a9416a6264d70e6740ff3b5
Taylor Niles <tniles320@gmail.com>
2020-11-05 23:06:24 -0800

    updated server.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,49 +5,49 @@ const router = express.Router();
 
 // home page route, displays all burgers from table
 router.get("/", (req, res) => {
-    burger.selectAll((data) => {
-      const burgerObject = {
-        burgers: data
-      };
-      res.render("index", burgerObject);
-    });
+  burger.selectAll((data) => {
+    const burgerObject = {
+      burgers: data
+    };
+    res.render("index", burgerObject);
   });
+});
   
-  // post route to add burger to table
-  router.post("/api/burgers", (req, res) => {
-    burger.insertOne([
-      "burger_name"
-    ], [
-      req.body.burger_name,
-    ], function(result) {
-      res.json({ id: result.insertId });
-    });
+// post route to add burger to table
+router.post("/api/burgers", (req, res) => {
+  burger.insertOne([
+    "burger_name"
+  ], [
+    req.body.burger_name,
+  ], function(result) {
+    res.json({ id: result.insertId });
   });
+});
   
-  // put route to update devoured from false to true
-  router.put("/api/burgers/:id", (req, res) => {
-    const condition = `id = ${req.params.id}`;  
-    burger.updateOne({
-      devoured: true
-    }, condition, function(result) {
-      if (result.changedRows == 0) {
-        return res.status(404).end();
+// put route to update devoured from false to true
+router.put("/api/burgers/:id", (req, res) => {
+  const condition = `id = ${req.params.id}`;  
+  burger.updateOne({
+    devoured: true
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+  
+// delete route to delete burger from table
+router.delete("/api/burgers/:id", (req, res) => {
+  const condition = `id = ${req.params.id}`
+  burger.delete(condition, (result) => {
+      if(result.affectedRows === 0) {
+          return res.status(404).end();
       } else {
-        res.status(200).end();
+          res.status(200).end();
       }
-    });
-  });
-  
-  // delete route to delete burger from table
-  router.delete("/api/burgers/:id", (req, res) => {
-    const condition = `id = ${req.params.id}`
-    burger.delete(condition, (result) => {
-        if(result.affectedRows === 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    })
-  });
+  })
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const router = require("./controllers/burgers_controller");
+const routes = require("./controllers/burgers_controller");
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,7 +14,7 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(router);
+app.use("/burgers_controllers", routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
