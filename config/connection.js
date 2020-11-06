defcfcb47f37590b55d3f73f9052779f71e94d04
Taylor Niles <tniles320@gmail.com>
2020-11-05 20:33:10 -0800

    updated mysql connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,20 @@
 const mysql = require("mysql");
 
+/*
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "password",
   database: "burgers_db"
+});
+*/
+
+const connection = mysql.createConnection({
+  host: "us-cdbr-iron-east-02.cleardb.net",
+  user: "b7e2437887xxxa",
+  password: "0200xxx6",
+  database: "heroku_7643ec736354xxx"
 });
 
 // create mysql connection
