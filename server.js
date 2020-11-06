20842841404334c800ca4424463c141d30d62ee6
Taylor Niles <tniles320@gmail.com>
2020-11-06 09:48:49 -0800

    updating server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const routes = require("./controllers/burgers_controller");
+const router = require("./controllers/burgers_controller");
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,7 +14,7 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use("/burgers_controllers", routes);
+app.use("/", router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
